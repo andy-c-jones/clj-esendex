7da8a8c73cdd2f315251a72cecfeb957262836cb
AJ <andrew_jones@outlook.com>
2016-01-11 16:21:52 +0000

    Begun the project

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clj-esendex "0.1.0-SNAPSHOT"
+  :description "Esendex Clojure SDK"
+  :url "https://github.com/andy-c-jones/clj-esendex"
+  :license {:name "MIT Licence"
+            :url "https://github.com/andy-c-jones/clj-esendex/blob/master/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [clj-http "2.0.0"]])
