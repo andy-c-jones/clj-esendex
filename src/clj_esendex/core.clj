2112318060e4aae623334cc02c14a5bbabb0b830
AJ <andrew_jones@outlook.com>
2016-01-12 09:33:14 +0000

    xml for a message forms correctly

--- src/clj_esendex/core.clj
+++ src/clj_esendex/core.clj
@@ -1,5 +1,5 @@
 (ns clj-esendex.core (:require [clojure.data.xml :as xml]))
 
 (defn create-sms-xml [to body]
-  (-> (xml/element :message)
+  (-> (xml/element :message {} (xml/element :to {} to) (xml/element :body {} body))
       (xml/emit-str)))
--- test/clj_esendex/core_test.clj
+++ test/clj_esendex/core_test.clj
@@ -4,4 +4,4 @@
             [clojure.data.xml :as xml]))
 
 (fact "message coverts to the correct xml"
-  (create-sms-xml 447987654321 "Hello World") => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><message></message>")
+  (create-sms-xml 447987654321 "Every message matters!") => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><message><to>447987654321</to><body>Every message matters!</body></message>")
