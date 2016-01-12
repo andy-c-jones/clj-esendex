8810a586c18384bbe2f42df0e0686392f367ce1c
AJ <andrew_jones@outlook.com>
2016-01-12 09:34:12 +0000

    renames test

--- test/clj_esendex/core_test.clj
+++ test/clj_esendex/core_test.clj
@@ -3,5 +3,5 @@
   (:require [clj-esendex.core :refer :all]
             [clojure.data.xml :as xml]))
 
-(fact "message coverts to the correct xml"
+(fact "creates the correct xml for a single message"
   (create-sms-xml 447987654321 "Every message matters!") => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><message><to>447987654321</to><body>Every message matters!</body></message>")
