c716bce16db49b4db46778a10974f4f3a427b6ed
AJ <andrew_jones@outlook.com>
2016-01-13 15:50:12 +0000

    Removed unneeded code

--- test/clj_esendex/utils_test.clj
+++
@@ -1,6 +0,0 @@
-(ns clj-esendex.utils-test
-  (:use midje.sweet)
-  (:require [clj-esendex.utils :refer :all]))
-
-(fact "base64-encode encodes a string as UTF-8 base64"
- (base64-encode "test@example.com:letmein") => "dGVzdEBleGFtcGxlLmNvbTpsZXRtZWlu")
