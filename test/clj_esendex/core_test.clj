44a723906b4d978f024cbca404f7d0ef8c1e2a43
AJ <andrew_jones@outlook.com>
2016-01-13 15:49:01 +0000

    Added integration tests that assert that basic auth and message xml is correctly send to esendex

--- test/clj_esendex/core_test.clj
+++ test/clj_esendex/core_test.clj
@@ -2,18 +2,34 @@
   (:require [clj-esendex.core :refer :all]
             [midje.sweet :refer :all]
             [ring.adapter.jetty-async :refer [run-jetty-async]]
-            [clojure.data.xml :as xml]))
+            [clojure.data.xml :as xml]
+            [ring.util.request :as request-util]))
 
 (fact "sms-xml creates the correct xml for a single message"
   (sms-xml "EX0000000" 447987654321 "Every message matters!") => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><messages><accountreference>EX0000000</accountreference><message><to>447987654321</to><body>Every message matters!</body></message></messages>")
 
-(defn app-handler [request]
-  {:status 200
-   :headers {"Content-Type" "application/xml"}
-   :body ""})
+(def ok {:status 200 :headers {"Content-Type" "application/xml"}})
+(def not-authorised {:status 200 :headers {"Content-Type" "application/xml"}})
 
-(fact "you can send an sms"
+(defn ok-with-request-for-test-assertions [request]
+  (->> request
+       request-util/body-string
+       (assoc ok :body)))
+
+(defn app-handler [request]
+  (let [response {:status 200 :headers {"Content-Type" "application/xml"}}])
+  (cond
+    (not= (get (:headers request) "authorization")"Basic dXNlcjpwYXNz") not-authorised
+    :else (ok-with-request-for-test-assertions request)))
+
+(facts "The post response function sends an sms and basic auth to the service"
   (let [fake (run-jetty-async app-handler {:join? false :port 3210})]
-  (Thread/sleep 1000)
-  (post-reponse "http://localhost:3210" "user" "pass" "body") => (contains {:status 200})
-  (.stop fake)))
+  (Thread/sleep 2000)
+
+(let [response (->> (sms-xml "EX123456" 447987654321 "Integration tests matter!")
+                    (post-reponse "http://localhost:3210" "user" "pass"))]
+    (fact "A post is made to the service with basic auth"
+      response => (contains {:status 200}))
+    (fact "The sms xml was correctly passed in the body of the request to the service"
+      (-> response :body) => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><messages><accountreference>EX123456</accountreference><message><to>447987654321</to><body>Integration tests matter!</body></message></messages>")
+  (.stop fake))))
